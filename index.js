86840feb73e7a301401e7e4677384c00ca52ae4a
Steven Trujillo <potencyalmighty@outlook.com>
2021-12-11 07:03:26 -0700

    first initial commit.

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const umm = document.querySelector('#form1')
+
+
+function whoa(){
+    let name = document.querySelector('#name');
+    
+}
+
+
+
+
+/*.addEventListener('submit', function(){
+    let name = document.querySelector('#name').value;
+    //alert('hello, ' + name);
+    //e.preventDefault();
+    document.querySelector('#form1').innerHTML = 'hello, ' + name;
+});
+
+
+</script>*/
